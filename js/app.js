7b3011b24e39dc8ab794e682423db93960ca603c
Jonathan Rumsey <ricochetrider@hotmail.com>
2022-03-10 15:38:24 -0800

    fixed 2 minor bugs. added functions to qs 1 thru 5.

--- js/app.js
+++ js/app.js
@@ -4,64 +4,84 @@
  let finalScore = 0;
 
 // //Question 1 ---------------------------------------------------------------------
-let question1 = prompt('Do I have a degree with a minor in web design?').toLowerCase();
+function questionOne() {
+  let question1 = prompt('Do I have a degree with a minor in web design?').toLowerCase();
 
-if(question1 === 'y' || question1 === 'yes'){
-  //console.log('I do indeed have a degree with a minor in web design. Unfortunately it didn\'t give me coding experience.');
-  alert('I do indeed have a degree with a minor in web design. Unfortunately it didn\'t give me coding experience.');
-  finalScore++;
-} else {
-  //console.log('Well, I actually do have a degre with a minor in web design!');
-  alert('Well, I actually do have a degre with a minor in web design!');
-} 
+  if (question1 === 'y' || question1 === 'yes') {
+    //console.log('I do indeed have a degree with a minor in web design. Unfortunately it didn\'t give me coding experience.');
+    alert('I do indeed have a degree with a minor in web design. Unfortunately it didn\'t give me coding experience.');
+    finalScore++;
+  } else {
+    //console.log('Well, I actually do have a degre with a minor in web design!');
+    alert('Well, I actually do have a degre with a minor in web design!');
+  }
+}
+
+questionOne();
 
 // //Question 2 ---------------------------------------------------------------------
-let question2 = prompt('Is my favorite drink chocolate mile?').toLowerCase();
+function questionTwo() {
+  let question2 = prompt('Is my favorite drink chocolate milk?').toLowerCase();
 
-if(question2 === 'y' || question2 === 'yes'){
-  //console.log('Heck yeah! Chocolate milk is the best! :D');
-  alert('Heck yeah! Chocolate milk is the best! :D');
-  finalScore++;
-} else {
-  //console.log('Oof, there\'s no way I wouldn\'t like chocolate milk :( ');
-  alert('Oof, there\'s no way I wouldn\'t like chocolate milk :( ')
+  if(question2 === 'y' || question2 === 'yes'){
+    //console.log('Heck yeah! Chocolate milk is the best! :D');
+    alert('Heck yeah! Chocolate milk is the best! :D');
+    finalScore++;
+  } else {
+    //console.log('Oof, there\'s no way I wouldn\'t like chocolate milk :( ');
+    alert('Oof, there\'s no way I wouldn\'t like chocolate milk :( ')
+  }    
 }
+
+questionTwo();
 
 // //Question 3 ---------------------------------------------------------------------
-let question3 = prompt('Is my favorite game Guild Wars 2?').toLowerCase();
+function questionThree() {
+  let question3 = prompt('Is my favorite game Guild Wars 2?').toLowerCase();
 
-if(question3 === 'y' || question3 === 'yes'){
-  //console.log('Yes! Guild Wars 2 has been my favorite game for 2 weeks now.');
-  alert('Yes! Guild Wars 2 has been my favorite game for 2 weeks now.');
-  finalScore++;
-} else {
-  //console.log('Wrong! It actually is my favorite game for the past 2 weeks');
-  alert('Wrong! It actually is my favorite game for the past 2 weeks');
+  if(question3 === 'y' || question3 === 'yes'){
+    //console.log('Yes! Guild Wars 2 has been my favorite game for 2 weeks now.');
+    alert('Yes! Guild Wars 2 has been my favorite game for 2 weeks now.');
+    finalScore++;
+  } else {
+    //console.log('Wrong! It actually is my favorite game for the past 2 weeks');
+    alert('Wrong! It actually is my favorite game for the past 2 weeks');
+  }  
 }
+
+questionThree();
 
 // //Question 4 ---------------------------------------------------------------------
-let question4 = prompt('Did I live in Japan for 5 years?').toLowerCase();
+function questionFour() {
+  let question4 = prompt('Did I live in Japan for 5 years?').toLowerCase();
 
-if(question4 === 'y' || question4 === 'yes'){
-  //console.log('That\'s right! I spend 5 years living in Okinawa, Japan.');
-  alert('That\'s right! I spend 5 years living in Okinawa, Japan.');
-  finalScore++;
-} else {
-  //console.log('No, no! I did spend 5 years in Japan.');
-  alert('No, no! I did spend 5 years in Japan.');
+  if(question4 === 'y' || question4 === 'yes'){
+    //console.log('That\'s right! I spend 5 years living in Okinawa, Japan.');
+    alert('That\'s right! I spend 5 years living in Okinawa, Japan.');
+    finalScore++;
+  } else {
+    //console.log('No, no! I did spend 5 years in Japan.');
+    alert('No, no! I did spend 5 years in Japan.');
+  }
 }
+
+questionFour();
 
 // //Question 5 ---------------------------------------------------------------------
-let question5 = prompt('Do I like Kpop?').toLowerCase();
+function questionFive() {
+  let question5 = prompt('Do I like Kpop?').toLowerCase();
 
-if(question5 === 'y' || question5 === 'yes'){
-  //console.log('Yeah, I think it\s pretty good. It\'s got some positive vibes.');
-  alert('Yeah, I think it\s pretty good. It\'s got some positive vibes.');
-  finalScore++;
-} else {
-  //console.log('I actually do. It\s got some positive vibes.');
-  alert('I actually do. It\s got some positive vibes.');
+  if (question5 === 'y' || question5 === 'yes') {
+    //console.log('Yeah, I think it\s pretty good. It\'s got some positive vibes.');
+    alert('Yeah, I think it\s pretty good. It\'s got some positive vibes.');
+    finalScore++;
+  } else {
+    //console.log('I actually do. It\s got some positive vibes.');
+    alert('I actually do. It\s got some positive vibes.');
+  }
 }
+
+questionFive();
 
 //Name greeting ---------------------------------------------------------------------
 let nameQuestion = prompt('Please tell me your name :)');
@@ -103,26 +123,26 @@ while (numGuesses < 6) {
   let correct = false;
 
   for (let i = 0; i < myArray.length; i++) {
-      if (arrayGuess === myArray[i]) {
-        i = myArray.length;
-        numGuesses = 6;
-        correct = true;
-      } else if (arrayGuess !== myArray[i]) {
+    if (arrayGuess === myArray[i]) {
+      i = myArray.length;
+      numGuesses = 6;
+      correct = true;
+    } else if (arrayGuess !== myArray[i]) {
 
-      }
     }
-      if (correct) {
-        alert(`That\'s correct! I like ${myArray[0]}, ${myArray[1]}, ${myArray[2]}, ${myArray[3]}, `);
-        finalScore++;
-      } else {
-        alert('Not quite~ Try again');
-      }
+  }
+  if (correct) {
+    alert(`That\'s correct! I like ${myArray[0]}, ${myArray[1]}, ${myArray[2]}, ${myArray[3]}`);
+    finalScore++;
+  } else {
+    alert('Not quite~ Try again');
+  }
 
   numGuesses++
   if (numGuesses === 6) {
     alert(`Better luck next time. You ran out of guesses. I like ${myArray[0]}, ${myArray[1]}, ${myArray[2]}, ${myArray[3]}`)
   }
-} 
+}
 
 alert(`Lastly, you were being scored on the number of correct answers. Final Score: ${finalScore}`);
 
